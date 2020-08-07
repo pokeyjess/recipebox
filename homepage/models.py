5b2677180cdf9b577a0497dd6ed4842f9d7f5329
Jessica Benson <pokeyjess72@gmail.com>
2020-08-07 16:11:42 -0400

    Completes main page with list of recipes

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+#from django.utils import timezone
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=80)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField()
+    #was body...
+    time_required = models.CharField(max_length=50)
+    instructions = models.TextField()
+    #post_date = models.DateTimeField(default=timezone.now)
+    
+#cascade, if we delete author, we delete all the items associated with that author
+
+    def __str__(self):
+        return f"{self.title} - {self.author.name}"
+
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from homepage.models import Recipe
 
-# Create your views here.
+
+def index(request):
+    my_recipes = Recipe.objects.all()
+    return render(request, "index.html", {"recipes": my_recipes, "title": " Recipes"})
+
+#def post_detail(request, post_id):
+    #my_recipe = Recipe.objects.filter(id=post_id).first()
+    #return render(request, "recipe.html", {"post": my_recipe})
+
