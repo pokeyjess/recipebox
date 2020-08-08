fdc60a930d997f5035282ed7a46845d83cf788af
Jessica Benson <pokeyjess72@gmail.com>
2020-08-07 21:16:42 -0400

    Adds author detail page

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from homepage.models import Recipe
+from homepage.models import Recipe, Author
 
 
 def index(request):
@@ -10,3 +10,6 @@ def recipe(request, recipe_id):
     my_recipe = Recipe.objects.filter(id=recipe_id).first()
     return render(request, "recipe.html", {"recipe": my_recipe})
 
+def author(request, author_id):
+    author_info = Author.objects.filter(id=author_id).first()
+    return render(request, "author.html", {"author": author_info})
--- recipebox/urls.py
+++ recipebox/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from homepage.views import index, recipe
+from homepage.views import index, recipe, author
 
 urlpatterns = [
     path('', index),
     path('recipe/<int:recipe_id>/', recipe),
+    path('author/<int:author_id>/', author),
     path('admin/', admin.site.urls),
 ]
