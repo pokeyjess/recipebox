e56efeae47f5ee29d13e205ad11797b72dd47a59
Jessica Benson <pokeyjess72@gmail.com>
2020-08-07 23:33:32 -0400

    Cleanup and minor fix for rubric

--- homepage/models.py
+++ homepage/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from django.utils import timezone
+
 
 class Author(models.Model):
     name = models.CharField(max_length=80)
@@ -8,17 +8,13 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     description = models.TextField()
-    #was body...
     time_required = models.CharField(max_length=50)
     instructions = models.TextField()
-    #post_date = models.DateTimeField(default=timezone.now)
-    
-#cascade, if we delete author, we delete all the items associated with that author
 
     def __str__(self):
         return f"{self.title} - {self.author.name}"
-
