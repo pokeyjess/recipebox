76c77c457d3f36bb90e4b55d8104e2c79e75d1c7
Jessica Benson <pokeyjess72@gmail.com>
2020-08-11 00:55:11 -0400

    Creates forms to add authors and recipes

--- recipebox/urls.py
+++ recipebox/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 # from django.conf import settings
 # from django.conf.urls.static import static
 from django.urls import path
-from homepage.views import index, recipe, author
+from homepage.views import index, recipe, author, add_recipe, add_author
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="homepage"),
     path('recipe/<int:recipe_id>/', recipe),
     path('author/<int:author_id>/', author),
+    path('newrecipe/', add_recipe, name="newrecipe"),
+    path('newauthor/', add_author, name="newauthor"),
     path('admin/', admin.site.urls),
 ]
